204c9f425fdace33c1cd2fba298179c7b0aad18a
Titov Ivan <risenforce@mail.ru>
2018-10-08 08:33:15 +0500

    add const

--- vex/include/aangle.h
+++ vex/include/aangle.h
@@ -2,13 +2,13 @@
 #define _aangle_
 
 float
-aangle(vector2 vec1; vector2 vec2)
+aangle(const vector2 vec1; const vector2 vec2)
 {
 	return acos(dot(vec1, vec2) / (length(vec1) * length(vec2));
 }
 
 float
-aangle(vector vec1; vector vec2)
+aangle(const vector vec1; const vector vec2)
 {
 	return acos(dot(vec1, vec2) / (length(vec1) * length(vec2));
 }
