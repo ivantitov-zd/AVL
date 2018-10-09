7e24cd266d201208bf578c2dcc5c1f8dd13bf536
Titov Ivan <risenforce@mail.ru>
2018-10-09 09:34:33 +0500

    Update vex/include/aangle.h
    fixed #8

--- vex/include/aangle.h
+++ vex/include/aangle.h
@@ -4,13 +4,13 @@
 float
 aangle(const vector2 vec1; const vector2 vec2)
 {
-	return acos(dot(vec1, vec2) / (length(vec1) * length(vec2));
+	return acos(dot(vec1, vec2) / (length(vec1) * length(vec2)));
 }
 
 float
 aangle(const vector vec1; const vector vec2)
 {
-	return acos(dot(vec1, vec2) / (length(vec1) * length(vec2));
+	return acos(dot(vec1, vec2) / (length(vec1) * length(vec2)));
 }
 
 #endif
