0642542cd420855c156aa456b62105b9a8e83e9f
Titov Ivan <risenforce@mail.ru>
2018-11-29 23:20:36 +0500

    amedian amedianlow amedianhigh
    fixed #15
    resolved #35 #36

--- vex/include/amedian.h
+++ vex/include/amedian.h
@@ -7,11 +7,9 @@ amedian(const float array_[])
     float sorted[] = sort(array_);
     int arrayLength = len(sorted);
     if (arrayLength % 2 == 0)
-    {
-        return avg(sorted[arrayLength/2-1], sorted[arrayLength/2]);
-    } else {
+        return avg(sorted[(arrayLength-1)/2], sorted[arrayLength/2]);
+    else
         return sorted[arrayLength/2];
-    }
 }
 
 float
@@ -20,11 +18,37 @@ amedian(const int array_[])
     int sorted[] = sort(array_);
     int arrayLength = len(sorted);
     if (arrayLength % 2 == 0)
-    {
-        return avg(sorted[arrayLength/2-1], sorted[arrayLength/2]);
-    } else {
+        return avg(sorted[(arrayLength-1)/2], sorted[arrayLength/2]);
+    else
         return sorted[arrayLength/2];
-    }
+}
+
+float
+amedianlow(const float array_[])
+{
+    float sorted[] = sort(array_);
+    return sorted[(len(sorted)-1)/2];
+}
+
+int
+amedianlow(const int array_[])
+{
+    int sorted[] = sort(array_);
+    return sorted[(len(sorted)-1)/2];
+}
+
+float
+amedianhigh(const float array_[])
+{
+    float sorted[] = sort(array_);
+    return sorted[len(sorted)/2];
+}
+
+int
+amedianhigh(const int array_[])
+{
+    int sorted[] = sort(array_);
+    return sorted[len(sorted)/2];
 }
 
 #endif
